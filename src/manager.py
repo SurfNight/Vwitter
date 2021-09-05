11df440c29d52e14e8a555583fc2ad97af82092d
Thiago Guimarães <thiagoguimaraes.dev@gmail.com>
2021-09-04 23:32:58 -0300

    clean manager

--- src/manager.py
+++ src/manager.py
@@ -1,4 +1,4 @@
-from typing import Tuple, Dict
+from typing import Tuple
 
 class Manager():
     def __init__(self):
@@ -8,14 +8,9 @@ class Manager():
         self.commands.append(command)
     
     def find_matching_command_and_text(self, text: str) -> Tuple:
-        print("validando texto")
         text_start = text[:10]
-        breakpoint()
         for command in self.commands:
-            print(command)
             for trigger in command.triggers:
-                print(trigger)
                 if trigger in text_start:
-                    print(f"e o trigger: {trigger}")
                     return (command, text.replace(trigger, '', 1))
         raise LookupError("No command found")
